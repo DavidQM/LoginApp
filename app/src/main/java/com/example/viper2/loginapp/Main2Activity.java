af71d5cc269954ef435795337ccb74ed97310f92
DavidQM <alexis.quintero@udea.edu.co>
2017-05-15 19:00:43 -0500

    commit 2 - login facebook con firebase

--- app/src/main/java/com/example/viper2/loginapp/Main2Activity.java
+++ app/src/main/java/com/example/viper2/loginapp/Main2Activity.java
@@ -9,13 +9,17 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
+import com.facebook.Profile;
 import com.facebook.login.LoginManager;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.w3c.dom.Text;
 
 public class Main2Activity extends AppCompatActivity {
 
-    TextView tUsuario,tCorreo;
+    TextView tUsuario,tCorreo,tUid;
     Button bCerrar;
 
     @Override
@@ -25,12 +29,21 @@ public class Main2Activity extends AppCompatActivity {
 
         tUsuario = (TextView) findViewById(R.id.tUsuario);
         tCorreo = (TextView) findViewById(R.id.tCorreo);
+        tUid = (TextView) findViewById(R.id.tUid);
         bCerrar = (Button) findViewById(R.id.bCerrar);
 
+        /*
+        //DAtos del perfl de facebook
         Bundle bundle= getIntent().getExtras();
-
         tUsuario.setText(bundle.getString("name"));
         tCorreo.setText(bundle.getString("ID"));
+        */
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        tUsuario.setText(user.getDisplayName());
+        tCorreo.setText(user.getEmail());
+        tUid.setText(user.getUid());//nombre qye da firebse a la identificacion
+
         /*
         if (AccessToken.getCurrentAccessToken() == null){
             goMainActivity();
@@ -40,6 +53,7 @@ public class Main2Activity extends AppCompatActivity {
 
     public void logout(View view) {
         LoginManager.getInstance().logOut();
+        FirebaseAuth.getInstance().signOut();
         goMainActivity();
     }
 
--- app/src/main/java/com/example/viper2/loginapp/MainActivity.java
+++ app/src/main/java/com/example/viper2/loginapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.viper2.loginapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,15 @@ import com.facebook.FacebookSdk;
 import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
 
@@ -22,10 +32,43 @@ public class MainActivity extends AppCompatActivity {
     private LoginButton loginButton;
     private CallbackManager callbackManager;
 
+    //metodo de autentifi
+    private FirebaseAuth mAuth;
+    //metodo lisener
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    String TAG= "Firebase";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    goMainActivity2();
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        /*
+        //trae la instancia de la base dedatos
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //me paro en la referencia en odnde quiero poner el mensaje
+        DatabaseReference myRef = database.getReference("message");
+        //el dato a a lmacenar en la referencia
+        myRef.setValue("Hello, World!");
+        */
 
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions(Arrays.asList("email"));
@@ -41,7 +84,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
               //logeo exitoso
-                goMainActivity2();
+                handleFacebookAccessToken(loginResult.getAccessToken());
+                //goMainActivity2();
             }
 
             @Override
@@ -58,6 +102,21 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        AuthCredential credential= FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Error en login", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -72,8 +131,23 @@ public class MainActivity extends AppCompatActivity {
        // Log.d("name",name);
 
         Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        /*
         intent.putExtra("name",perfil.getName());
-        intent.putExtra("ID",perfil.getId());
+        intent.putExtra("ID",perfil.getId());*/
         startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
